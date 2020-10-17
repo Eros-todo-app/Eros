172b7aaab8affae62c9edf44eed0d57a69c53ca8
Eddie Englund <properllerbucher@gmail.com>
2020-10-17 23:10:53 +0200

    Created helper function for login and registration

--- src/routes/user/helpers/verifyUserInput.js
+++ src/routes/user/helpers/verifyUserInput.js
@@ -1,14 +1,32 @@
-const verifyRegistrationInput = (_email, _password, _name) => {
+const emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function verifyEmail(_email) {
   return new Promise((resolve, reject) => {
     const email = _email.toLowerCase().trim();
+    if (!emailReg.test(email) || email.length < 3) return reject("Invalid email address");
+    resolve(email);
+  });
+}
+
+function verifyPassword(_password) {
+  return new Promise((resolve, reject) => {
     const password = _password.trim();
+    if (password.length < 6 || password.legnth > 128)
+      return reject("Password must be at least 6 characters and a maximum of 128 characters");
+    resolve(password);
+  });
+}
+
+const verifyRegistrationInput = (_email, _password, _name) => {
+  return new Promise(async (resolve, reject) => {
     const name = _name.trim();
 
-    const emailReg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if (!emailReg.test(email) || email.length < 3) return reject("Invalid email address");
-
-    if (!password.length > 6 || !password.legnth < 128)
-      return reject("Password must be at least 6 characters and a maximum of 128 characters");
+    try {
+      await verifyEmail(_email);
+      verifyPassword(_password);
+    } catch (error) {
+      reject(error);
+    }
 
     if (!name.length > 1) return reject("Did you forget your name, ooor do you not have one?");
 
@@ -16,4 +34,17 @@ const verifyRegistrationInput = (_email, _password, _name) => {
   });
 };
 
-module.exports = { verifyRegistrationInput };
+const verifyLoginInput = (_email, _password) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const email = await verifyEmail(_email);
+      const password = await verifyPassword(_password);
+
+      resolve({ email, password });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+module.exports = { verifyRegistrationInput, verifyLoginInput };
