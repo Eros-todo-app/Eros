4aab28ca880598ed85a8af936ed8cd2de645f31f
Eddie Englund <properllerbucher@gmail.com>
2020-10-17 16:49:22 +0200

    Fixed mistake in db.js

--- src/db/db.js
+++ src/db/db.js
@@ -1,4 +1,4 @@
-const { MongoClient, connection } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const config = require("./config.json");
 
 class Database {
@@ -8,25 +8,13 @@ class Database {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-
-    this.connection = connection;
-
-    this.connection.on("connected", () =>
-      console.log(`Connected to databse: ${this.db_name}`)
-    );
-
-    this.connection.on("error", (err) => console.error(err));
-
-    this.connection.on("disconnected", () => {
-      console.warn("Database disconected, shutting down...");
-      process.exit(1);
-    });
   }
 
   connect() {
     return new Promise((resolve, reject) => {
       this.rootClient.connect((err, client) => {
         if (err) return reject("Failed to connect to Databse", err);
+        console.log(`Connected to databse: ${this.db_name}`);
 
         this.db = client.db(this.db_name);
         this.users = this.db.collection(config.collections.users);
