6beb4ee67eab1acbf1a1b5bfc328a69a54681e57
Eddie Englund <properllerbucher@gmail.com>
2020-10-17 23:11:35 +0200

    Added new routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,8 @@
 const userRegisterRouter = require("./user/register");
+const userLoginRouter = require("./user/login");
 
 const setupRoutes = (app) => {
-  app.use("/api/user/", userRegisterRouter);
+  app.use("/api/user/", userRegisterRouter, userLoginRouter);
 };
 
 module.exports = { setupRoutes };
