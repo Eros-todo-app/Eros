7ff98efd9aeff75be7af1a5cdf0b731a7bc9889d
Eddie Englund <properllerbucher@gmail.com>
2020-10-17 16:49:44 +0200

    registration route

---
+++ src/routes/user/register.js
@@ -0,0 +1,33 @@
+const { getDB } = require("../../db/db");
+const router = require("express").Router();
+const { verifyRegistrationInput } = require("./helpers/verifyUserInput");
+const errors = require("../../errors/errors.json");
+
+router.post("/register", async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const input = await verifyRegistrationInput(email, password, name);
+
+    const emailExist = await getDB().users.findOne({ email: input.email });
+    if (emailExist) return res.status(400).send({ field: "email", ...errors.BAD_DATA });
+
+    const newUser = await getDB().users.insertOne({
+      ...input,
+    });
+
+    return res.status(200).send({
+      success: true,
+      message: `You made it ${
+        newUser.ops[0].name.split(" ")[0]
+      }, you're now a registerd user! Please, make sure to check your email to verify your account!`,
+      user: {
+        id: newUser.ops[0]._id,
+        name: newUser.ops[0].name,
+      },
+    });
+  } catch (field) {
+    return res.status(400).send({ field, ...errors.BAD_DATA });
+  }
+});
+
+module.exports = router;
