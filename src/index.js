270381c35d6d94122c4b9b803980130c805c0916
Eddie Englund <properllerbucher@gmail.com>
2020-10-17 16:50:05 +0200

    Updated with init and other

--- src/index.js
+++ src/index.js
@@ -2,19 +2,30 @@ const express = require("express");
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const { MissingEnvError } = require("./errors/env.js");
-
-// Init
-
+const { setupRoutes } = require("./routes/routes");
+const { setupDB } = require("./db/db");
 dotenv.config();
-const app = express();
 
-const PORT = process.env.PORT;
+const init = async () => {
+  const MONGO_URI = process.env.MONGO_URI;
+  const DB_NAME = process.env.DB_NAME;
+  const PORT = process.env.PORT;
 
-// Middleware and config
+  if (!MONGO_URI) throw new MissingEnvError("MONGO_URI");
+  if (!DB_NAME) throw new MissingEnvError("DB_NAME");
+  if (!PORT) throw new MissingEnvError("PORT");
 
-app.use(express.json());
-app.use(helmet);
+  await setupDB(MONGO_URI, DB_NAME);
+  const app = express();
 
-if (!PORT) throw new MissingEnvError("PORT");
+  // Middleware and config
 
-app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+  app.use(express.json({ limit: "100kb", strict: true, type: "application/json" }));
+  app.use(helmet());
+
+  setupRoutes(app);
+
+  app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
+};
+
+init().catch((err) => console.log(err));
