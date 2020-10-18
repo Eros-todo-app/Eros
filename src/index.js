9c3528ff21c7181982653e89d36a22ac6adf75db
Eddie Englund <properllerbucher@gmail.com>
2020-10-18 21:04:03 +0200

    moved most index.js to an init function

--- src/index.js
+++ src/index.js
@@ -1,54 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const helmet = require("helmet");
-const session = require("express-session");
-const cookieParser = require("cookie-parser");
-const { v4: uuidv4 } = require("uuid");
-const { MissingEnvError } = require("./errors/env.js");
-const { setupRoutes } = require("./routes/routes");
-const { setupDB } = require("./db/db");
+const { init } = require("./setup/init");
 dotenv.config();
 
-const init = async () => {
-  const { SESSION_LIFETIME, MONGO_URI, DB_NAME, PORT = 3000, SESSION_SECRET, NODE_ENV, SID } = process.env;
-
-  if (!MONGO_URI) throw new MissingEnvError("MONGO_URI");
-  if (!DB_NAME) throw new MissingEnvError("DB_NAME");
-  if (!PORT) throw new MissingEnvError("PORT");
-  if (!SESSION_SECRET) throw new MissingEnvError("SESSION_SECRET");
-  if (!NODE_ENV) throw new MissingEnvError("NODE_ENV");
-  if (!SESSION_LIFETIME) throw new MissingEnvError("SESSION_LIFETIME");
-  if (!SID) throw new MissingEnvError("SID");
-
-  await setupDB(MONGO_URI, DB_NAME);
-  const app = express();
-
-  const IS_PROD = NODE_ENV === "production";
-
-  // Middleware and config
-  app.use(express.json({ limit: "100kb", strict: true, type: "application/json" }));
-  app.use(helmet());
-  app.use(cookieParser());
-  app.use(
-    session({
-      secret: SESSION_SECRET,
-      secure: IS_PROD,
-      resave: false,
-      saveUninitialized: true,
-      cookie: {
-        maxAge: parseInt(SESSION_LIFETIME),
-        sameSite: true,
-        secure: IS_PROD,
-      },
-      genid: function () {
-        return uuidv4();
-      },
-    })
-  );
-
-  setupRoutes(app);
-
-  app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+const app = express();
+const start = async () => {
+  await init(app);
 };
 
-init().catch((err) => console.log(err));
+start().catch((err) => console.log(err));
---
+++ src/setup/init.js
@@ -0,0 +1,53 @@
+const helmet = require("helmet");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const { v4: uuidv4 } = require("uuid");
+const { MissingEnvError } = require("../errors/env.js");
+const { setupRoutes } = require("../routes/routes");
+const { setupDB } = require("../db/db");
+const express = require("express");
+
+// TODO: Add redis store for sessions
+
+const init = async (app) => {
+  const { SESSION_LIFETIME, MONGO_URI, DB_NAME, PORT = 3000, SESSION_SECRET, NODE_ENV, SID } = process.env;
+
+  if (!MONGO_URI) throw new MissingEnvError("MONGO_URI");
+  if (!DB_NAME) throw new MissingEnvError("DB_NAME");
+  if (!PORT) throw new MissingEnvError("PORT");
+  if (!SESSION_SECRET) throw new MissingEnvError("SESSION_SECRET");
+  if (!NODE_ENV) throw new MissingEnvError("NODE_ENV");
+  if (!SESSION_LIFETIME) throw new MissingEnvError("SESSION_LIFETIME");
+  if (!SID) throw new MissingEnvError("SID");
+
+  await setupDB(MONGO_URI, DB_NAME);
+
+  const IS_PROD = NODE_ENV === "production";
+
+  // Middleware and config
+  app.use(express.json({ limit: "100kb", strict: true, type: "application/json" }));
+  app.use(helmet());
+  app.use(cookieParser());
+  app.use(
+    session({
+      secret: SESSION_SECRET,
+      secure: IS_PROD,
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: parseInt(SESSION_LIFETIME),
+        sameSite: true,
+        secure: IS_PROD,
+      },
+      genid: function () {
+        return uuidv4();
+      },
+    })
+  );
+
+  setupRoutes(app);
+
+  app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
+};
+
+module.exports = { init };
