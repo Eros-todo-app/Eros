d969ea6f454936c079098a2917e4406ce7af3d85
Eddie Englund <properllerbucher@gmail.com>
2020-10-20 11:31:39 +0200

    Fixed verifyUser

--- src/middleware/verifyUser.js
+++ src/middleware/verifyUser.js
@@ -1,9 +1,8 @@
 const errors = require("../errors/errors.json");
 
-const verifyUser = (req, res, next) => {
-  if (!req.session.user.loggedIn) res.status(400).send(...errors.BAD_AUTH);
-
-  next();
-};
+function verifyUser(req, res, next) {
+  if (!req.session.user) return res.status(400).send({ ...errors.BAD_AUTH });
+  return next();
+}
 
 module.exports = { verifyUser };
