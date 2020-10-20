2439ec287f9f1580c374f90f3c6f64b4e0ee45c6
Eddie Englund <properllerbucher@gmail.com>
2020-10-20 11:32:51 +0200

    Added logout and createTodo to routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,11 @@
 const userRegisterRouter = require("./user/register");
 const userLoginRouter = require("./user/login");
+const userLogoutRouter = require("./user/logout");
+const createTodoRouter = require("./todos/createTodo");
 
 const setupRoutes = (app) => {
-  app.use("/api/user/", userRegisterRouter, userLoginRouter);
+  app.use("/api/user/", userRegisterRouter, userLoginRouter, userLogoutRouter);
+  app.use("/api/todos/", createTodoRouter);
 };
 
 module.exports = { setupRoutes };
