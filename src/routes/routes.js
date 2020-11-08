8dbbe9150125592678911e4f55ac570553073408
Eddie Englund <properllerbucher@gmail.com>
2020-11-08 18:29:47 +0100

    added deleteTodo to the router

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,10 +2,12 @@ const userRegisterRouter = require("./user/register");
 const userLoginRouter = require("./user/login");
 const userLogoutRouter = require("./user/logout");
 const createTodoRouter = require("./todos/createTodo");
+const editTodoRouter = require("./todos/editTodo");
+const deleteTodoRouter = require("./todos/deleteTodo");
 
 const setupRoutes = (app) => {
   app.use("/api/user/", userRegisterRouter, userLoginRouter, userLogoutRouter);
-  app.use("/api/todos/", createTodoRouter);
+  app.use("/api/todos/", createTodoRouter, editTodoRouter, deleteTodoRouter);
 };
 
 module.exports = { setupRoutes };
